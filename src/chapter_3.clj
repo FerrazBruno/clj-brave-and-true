ca1f7051ce6158d5dd1f659547f1ae89f700c3ba
Bruno Ferraz <brunohenriqueferraz94@gmail.com>
2025-01-20 07:34:11 -0300

    Anonymous Functions

--- src/chapter_3.clj
+++ src/chapter_3.clj
@@ -153,4 +153,212 @@
   {:first-name "Charlie"
    :last-namee "McFishwich"}
   {"string-key" +}
+  {:name {:first "John" :middle "Jacob" :last "Jingleheimerschmidt"}}
+  (hash-map :a 1 :b 2)
+  (get {:a 0 :b 1} :a)
+  (get {:a 0 :b {:c "ho hum"}} :b)
+  (get {:a 0 :b 1} :c)
+  (get {:a 0 :b 1} :c "unicorns?")
+  (get-in {:a 0 :b {:c "ho hum"}} [:b :c])
+  )
+
+;; Keywords
+(comment
+  :a
+  :rumplestiltsken
+  :34
+  :_?
+  (:a {:a 1 :b 2 :c 3})
+  (:d {:a 1 :b 2 :c 3} "No gnome knows homes likes Noah knows")
+  )
+
+;; Vectors
+(comment
+  [3 2 1]
+  (get [3 2 1] 0)
+  (get ["a" {:name "Pugsley Winterbotton"} "c"] 1)
+  (vector "creepy" "full" "moon")
+  (conj [1 2 3] 4)
+  )
+
+;; Lists
+(comment
+  '(1 2 3 4)
+  (nth '(1 2 3 4) 0)
+  (nth '(:a :b :c) 2)
+  (list 1 "two" {3 4})
+  (conj '(1 2 3) 4)
+  )
+
+;; Sets
+(comment
+  #{"kurt vonnegut" 20 :icicle}
+  (hash-set 1 1 2 2)
+  (conj #{:a :b} :b)
+  (set [3 3 3 4 4])
+  (contains? #{:a :b} :a)
+  (contains? #{:a :b} 3)
+  (contains? #{nil} nil)
+  (:a #{:a :b})
+  (get #{:a :b} :a)
+  (get #{:a nil} nil)
+  (get #{:a :b} "kurt vonnegut")
+  )
+
+;; Simplicity
+;; It is better to have 100 functions operate on one data structure than 10 functions on 10 data structures.
+;; Alan Perlis
+
+;; Functions
+;; Calling Functions
+(comment
+  (+ 1 2 3 4)
+  (* 1 2 3 4)
+  (first [1 2 3 4])
+  (or + -)
+  ((or + -) 1 2 3)
+  ((and (= 1 1) +) 1 2 3)
+  ((first [+ 0]) 1 2 3)
+  (inc 1.1)
+  (map inc [0 1 2 3])
+  )
+
+;; Functions Calls, Macro Calls, and Special Forms
+;; Special forms: They don't always evaluate all of their operands
+;; "if" is an example
+;; Can't use them as arguments to functions
+;; Macros also can't be passed as arguments to functions
+;; Defining Functions
+
+(comment
+  (defn too-enthusiastic
+    "Return a cheer chat might be a bit too enthusiastic"
+    [name]
+    (str "OH. MY. GOD! " name " YOU ARE THE MOST DEFINITELY LIKE THE BEST "
+         "MAN SLASH WOMAN EVER I LOVE YOU AND WE SHOULD RUN AWAY SOMEWHERE"))
+
+  (too-enthusiastic "Zelda")
+  )
+
+;; Parameters and Arity
+(comment
+  (defn no-params
+    []
+    "I take no parameters")
+  (defn one-param
+    [x]
+    (str "I take one parameter: " x))
+  (defn two-params
+    [x y]
+    (str "Two parameters! That's nothing! Pah! I will smoosh them "
+         "together to spit you! " x y))
+  ;; multiple-arity function
+  (defn multi-arity
+    ([first-arg second-arg third-arg]
+     #_(do-things first-arg second-arg third-arg))
+    ([first-arg second-arg]
+     #_(do-things first-arg second-arg))
+    ([first-arg]
+     #_(do-things first-arg)))
+
+  (defn x-chop
+    "Describe the kind of chop you're inflicting on someone"
+    ([name chop-type]
+     (str "I " chop-type " chop " name "! Take that!"))
+    ([name]
+     (x-chop name "karate")))
+
+  (x-chop "Kanye West" "slap")
+  (x-chop "Kanye West")
+
+  (defn weird-arity
+    ([]
+     "Destiny dressed you this morning, my friend, and now Fear is
+       trying to pull off your pants. If you give up, if you give in,
+       you're gonna end up naked with Fear just standing there laughing
+       at your dangling unmentionables! - the Tick")
+    ([number]
+      (inc number)))
+
+  (defn codger-communication
+    [whippersnapper]
+    (str "Get off my lawn, " whippersnapper "!!!"))
+
+  (defn codger
+    [& whippersnapper]
+    (map codger-communication whippersnapper))
+
+  (codger "Billy" "Anne-Marie" "The Incredible Bulk")
+
+  (defn favorite-things
+    [name & things]
+    (str "Hi, " name ", here are my favorite things: " (clojure.string/join ", " things)))
+
+  (favorite-things "Doreen" "gum" "shoes" "kara-te")
+  )
+
+;; Destructuring
+(comment
+  (defn my-first
+    [[first-thing]]
+    first-thing)
+
+  (my-first ["oven" "bike" "war-axe"])
+
+
+  (defn chooser
+    [[first-choice second-choice & unimportant-choices]]
+    (println (str "Your first choice is: " first-choice))
+    (println (str "Your second choice is: " second-choice))
+    (println (str "We're ignoring the rest of the your choices. "
+                  "Here they are in case you need to crie over them: "
+                  (clojure.string/join ", " unimportant-choices))))
+
+
+  (chooser ["Marmalade" "Handsome Jack" "Pigpen" "Aquaman"])
+
+
+  (defn announce-treasure-location
+    [{lat :lat lng :lng}]
+    (println (str "Treasure lat: " lat))
+    (println (str "Treasure lng: " lng)))
+
+  #_(announce-treasure-location {:lat 28.22 :lng 81.33})
+
+  (defn announce-treasure-location
+    [{:keys [lat lng]}]
+    (println (str "Treasure lat: " lat))
+    (println (str "Treasure lng: " lng)))
+
+  (defn announce-treasure-location
+    [{:keys [lat lng]} :as treasure-location]
+    (println (str "Treasure lat: " lat))
+    (println (str "Treasure lng: " lng)))
+  )
+
+;; Function Body
+(comment
+  (defn illustrative-function
+    []
+    (+ 1 304)
+    30
+    "joe")
+
+  (illustrative-function)
+
+  (defn number-comment
+    [x]
+    (if (> x 6)
+      "Oh my gosh! What a big number!"
+      "That number's OK, I guess"))
+
+  (number-comment 5)
+  (number-comment 7)
+  )
+
+;; Anonymous Functions
+(comment
+  (map (fn [name] (str "Hi, " name)) ["Darth Vader" "Mr. Magoo"])
+
+  ((fn [x] (* x 3)) 8)
   )
