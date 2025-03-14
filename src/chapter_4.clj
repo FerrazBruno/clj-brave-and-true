6db1d39dea3f54a91efc89b7c408776f7a3c2d96
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-03-13 22:51:12 -0300

    Lazy Seqs

--- src/chapter_4.clj
+++ src/chapter_4.clj
@@ -49,4 +49,48 @@
      {:alias "Easter Bunny" :real "Your dad"}])
   (map :real identities)
   ;; reduce
+  (reduce (fn [new-map [key val]]
+            (assoc new-map key (inc val)))
+          {}
+          {:max 30 :min 10})
+  (reduce (fn [new-map [key val]]
+            (if (> val 4)
+              (assoc new-map key val)
+              new-map))
+          {}
+          {:human 4.1
+           :critter 3.9})
+  ;; take, drop, take-while and drop-while
+  (take 3 [1 2 3 4 5 6 7 8 9 10])
+  (drop 3 [1 2 3 4 5 6 7 8 9 10])
+  (def food-journal
+    [{:month 1 :day 1 :human 5.3 :critter 2.3}
+     {:month 1 :day 2 :human 5.1 :critter 2.0}
+     {:month 2 :day 1 :human 4.9 :critter 2.1}
+     {:month 2 :day 2 :human 5.0 :critter 2.5}
+     {:month 3 :day 1 :human 4.2 :critter 3.3}
+     {:month 3 :day 5 :human 4.0 :critter 3.8}
+     {:month 4 :day 1 :human 3.7 :critter 3.9}
+     {:month 4 :day 4 :human 3.7 :critter 3.6}])
+  (take-while #(< (:month %) 3) food-journal)
+  (drop-while #(< (:month %) 3) food-journal)
+  (take-while #(< (:month %) 4)
+              (drop-while #(< (:month %) 2) food-journal))
+  ;; filter and some
+  (filter #(< (:human %) 5) food-journal)
+  (filter #(< (:month %) 3) food-journal)
+  (some #(> (:critter %) 5) food-journal)
+  (some #(> (:critter %) 3) food-journal)
+  (some #(and (> (:critter %) 3) %) food-journal)
+  ;; sort and sort-by
+  (sort [3 1 2])
+  (sort-by count ["aaa" "c" "bb"])
+  (sort-by :preco [{:preco 10} {:preco 1} {:preco 5}])
+  ;; concat
+  (concat [1 2] [3 4])
+  )
+
+;; Lazy Seqs
+(comment
+
   )
