14d96d1a2a43309adfea560c3cdb04b621e999d7
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-03-24 23:04:48 -0300

    Chapter 7: Clojure Alchemy: Reading, Evaluating and Macros

--- src/chapter_6.clj
+++ src/chapter_6.clj
@@ -7,8 +7,7 @@
   inc
   'inc
   (map inc [1 2])
-  '(map inc [1 2])
-)
+  '(map inc [1 2]))
 
 ;; Storing Objects With def
 (comment
@@ -26,8 +25,7 @@
   (def great-books ["The Power of Bees" "Journey of Upstairs"])
   great-books
 
-  (ns-interns *ns*)
-  )
+  (ns-interns *ns*))
 
 ;; Creating And Switching to Namespaces
 (comment
@@ -39,5 +37,41 @@
   cheddars
   cheese.analysis/cheddars
   ;; refer
-
+  (in-ns 'cheese.taxonomy)
+  (def cheddars ["mild" "medium" "strong" "sharp" "extra sharp"])
+  (def bries ["Wisconsin" "Somerset" "Brie de Meaux" "Brie de Melun"])
+  (in-ns 'cheese.analysis)
+  (clojure.core/refer 'cheese.taxonomy)
+  bries
+  (clojure.core/get (clojure.core/ns-map clojure.core/*ns*) 'bries)
+  (clojure.core/get (clojure.core/ns-map clojure.core/*ns*) 'cheddars)
+  (clojure.core/refer 'cheese.taxonomy :only ['bries])
+  bries
+  cheddars
+  (clojure.core/refer 'cheese.taxonomy :exclude ['bries])
+  bries
+  cheddars
+  (clojure.core/refer 'cheese.taxonomy :rename {'bries 'yummy-bries})
+  bries
+  ; yummy-bries
+  (in-ns 'cheese.analysis)
+  (defn- private-function
+    "Just an example function that does nothing"
+    [])
+  (in-ns 'cheese.taxonomy)
+  (clojure.core/refer-clojure)
+  (cheese.analysis/private-function)
+  (refer 'cheese.analysis :only ['private-function])
+  ;; alias
+  (clojure.core/alias 'taxonomy 'cheese.taxonomy)
+  taxonomy/bries
   )
+
+;; Real Project Organization
+
+;; The Relationship Between File Paths and Namespaces Names
+;; lein new app the-divine-cheese-code
+
+;; Requiring and Using Namespaces
+
+;; The ns Macro
