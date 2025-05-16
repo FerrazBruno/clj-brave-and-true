85c78dbced9a87516c8a7eb8d4235335d899d6c5
FerrazBruno <brunohenriqueferraz94@gmail.com>
2025-05-16 19:27:26 -0300

    Chapter 8: finished.

--- src/chapter_8.clj
+++ src/chapter_8.clj
@@ -133,4 +133,121 @@
   `(let [name# "Larry Potter"] name#)
 
   ;; Double Evaluation
-  )
+  (defmacro report [to-try]
+    `(if ~to-try
+       (println (quote ~to-try) "was successful:" ~to-try)
+       (println (quote ~to-try) "was not successful:" ~to-try)))
+  (report (do (Thread/sleep 1000) (+ 1 1)))
+  (defmacro report [to-try]
+    `(let [result# ~to-try]
+       (if ~to-try
+         (println (quote ~to-try) "was successful:" result#)
+         (println (quote ~to-try) "was not successful:" result#))))
+  (report (do (Thread/sleep 1000) (+ 1 1))))
+
+;; Macros All the Way Down
+(comment
+  (defmacro report [to-try]
+    `(let [result# ~to-try]
+       (if ~to-try
+         (println (quote ~to-try) "was successful:" result#)
+         (println (quote ~to-try) "was not successful:" result#))))
+  (report (= 1 1))
+  (report (= 1 2))
+  (doseq [code ['(= 1 1) '(= 1 2)]]
+    (report code))
+  (defmacro doseq-macro [macroname & args]
+    `(do
+       ~@(map (fn [arg] (list macroname arg)) args)))
+  (doseq-macro report (= 1 1) (= 1 2)))
+
+;; Brews for the Brave and True
+(comment
+  ;; Validation Functions
+  (def order-details
+    {:name "Mitchard Blimons"
+     :email "mitchard.blimonsgmail.com"})
+  order-details
+  (def order-details-validation
+    {:name  ["Please enter a name" not-empty]
+     :email ["Please enter an email address" not-empty
+
+             "Your email address doesn't look like an email address"
+             #(or (empty? %) (re-seq #"@" %))]})
+  (defn error-message-for
+    "Return a seq of error messages"
+    [to-validate message-validator-pairs]
+    (map first (filter #(not ((second %) to-validate))
+                       (partition 2 message-validator-pairs))))
+  (partition 2 (:email order-details-validation))
+  (error-message-for "abc.com" (:email order-details-validation))
+  (defn validate
+    "Returns a map with a vector of errors for each key"
+    [to-validade validations]
+    (reduce (fn [errors validations]
+              (let [[fieldname validation-check-groups] validations
+                    value (fieldname to-validade)
+                    error-messages
+                    (error-message-for value validation-check-groups)]
+                (if (empty? error-messages)
+                  errors
+                  (assoc errors fieldname error-messages))))
+            {}
+            validations))
+  (validate order-details order-details-validation)
+  ;; if-valid
+  (let [errors (validate order-details order-details-validation)]
+    (if (empty? errors)
+      (println :success)
+      (println :failure errors)))
+  (defn if-valid [record validations success-code failure-code]
+    (let [errors (validate record validations)]
+      (if (empty? errors)
+        success-code
+        failure-code)))
+  (defmacro if-valid [to-validate validations errors-name & then-else]
+    `(let [~errors-name (validate ~to-validate ~validations)]
+       (if (empty? ~errors-name)
+         ~@then-else)))
+  (macroexpand
+   '(if-valid order-details order-details-validation my-errors-name
+              (println :success)
+              (println :failure))))
+
+;; Exercises
+(comment
+  ;; 1.
+  (defmacro when-valid [to-validate validations & body]
+    `(when (~validations ~to-validate)
+       ~@body))
+  (defn valid? [x]
+    (= (:status x) :ok))
+  (def my-data {:status :ok})
+  (when-valid my-data valid?
+              (println :success)
+              (println :failure))
+  ;; 2.
+  (defmacro my-or
+    ([] nil)
+    ([x] x)
+    ([x & next]
+     `(let [or# ~x]
+        (if or# or# (or ~@next)))))
+  ;; 3.
+  (defmacro defattrs [& pairs]
+    (when (odd? (count pairs))
+      (throw (IllegalArgumentException. "defattrs requires an even number of arguments")))
+    `(do
+       ~@(for [[fname attr] (partition 2 pairs)]
+           `(defn ~fname [character#]
+              (~attr character#)))))
+  (defattrs
+    c-int :intelligence
+    c-str :strength
+    c-dex :dexterity)
+  (def character {:intelligence 12
+                  :strength 15
+                  :dexterity 10})
+  (c-int character)
+  (c-str character)
+  (c-dex character))
